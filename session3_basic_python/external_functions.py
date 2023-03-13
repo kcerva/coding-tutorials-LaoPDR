0d35303f235aa5d4ccf9fb8f6a79b2f51cbf0bad
kcerva <karcerbar@gmail.com>
2023-03-13 17:28:17 +0000

    Updated sessions 3 and 4

---
+++ session3_basic_python/external_functions.py
@@ -0,0 +1,16 @@
+# This file will be used for functions related to the best practices intro notebook
+def cost_calculation_external(qty,price):
+  """This function multiplies the quantity of items
+  by the unit prices and prints total cost.
+
+  Inputs
+  qty - item quantity
+  price - unit price
+
+  Outputs
+  Total cost print to screen
+  """
+  # Calculation
+  cost = qty * price
+  # Output
+  print(cost) 
