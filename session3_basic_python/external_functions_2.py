d644d711a737e009a6c746d5642db93bb6e1dfef
kcerva <karcerbar@gmail.com>
2023-03-22 21:01:07 +0000

    Updated final session excercises of floweaver tutorials

---
+++ session3_basic_python/external_functions_2.py
@@ -0,0 +1,17 @@
+# This script contains the functions used in session 3
+
+def cost_calculation_external(qty,price):   
+    """This function multiplies the quantity of items
+    by the unit prices and prints total cost.
+
+    Inputs
+    qty - item quantity
+    price - unit price
+
+    Outputs
+    Total cost print to screen
+    """
+    # Calculation
+    cost = qty * price
+    # Output
+    return cost
